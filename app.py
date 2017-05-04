6984577bf8e063c777f77c8908ac40c9d4c92113
Chaya16 <chaya.malik89@gmail.com>
2017-05-03 18:27:56 -0700

    minor changes

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, flash, render_template, redirect, url_for, request
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+import pdb
+
 
 # initate flask app
 app = Flask(__name__)
 
+# welcome page
+@app.route('/', methods=['GET'])
+def welcome():
+    error = None
+    return render_template('loginpage.html', error=error)
+    
+
 # route for handling the login page logic
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        if request.form['uname'] != 'admin' or request.form['pwd'] != 'admin':
+            error = 'Invalid Username/Password.'
         else:
-            return render_template('tenantspage.html')
+            return redirect(url_for('tenantspage'))
     return render_template('loginpage.html', error=error)
 
 # route for handling the tenants page
@@ -23,3 +35,4 @@ def tenantspage():
 # run app service 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000, debug=True)
+	
